f2295898125696f3e8efe2f42534a833aa1d8b8d
Cesar Gomez <zootechdrum@gmail.com>
2020-02-13 20:20:23 -0800

    added global font to app in app.js

--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -5,10 +5,14 @@ import Logo from "../../images/pokeball.png"
 function Navbar() {
     return (
         <nav className="navbar fixed-top">
-            <img className = "logo" src = {Logo} alt="pokeboll" />
+
+            <img className="logo" src={Logo} alt="pokeboll" />
+            <span class="navbar-text">
+                Pokemon Guess Game
+    </span>
 
         </nav>
-            )
-        }
-        
+    )
+}
+
 export default Navbar;
