4d6a6c334f203214a9b8eabe8a93b7f70028a8eb
Cesar Gomez <zootechdrum@gmail.com>
2020-02-13 19:36:01 -0800

    added pokeball image

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+    <Navbar />
     <Jumbotron />
 
     </div>
--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -1,2 +1,14 @@
 import React from "react";
 import "./style.css";
+import Logo from "../../images/pokeball.png"
+
+function Navbar() {
+    return (
+        <nav className="navbar">
+            <img className = "logo" src = {Logo} alt="pokeboll" />
+
+        </nav>
+            )
+        }
+        
+export default Navbar;
