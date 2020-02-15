f57a570db8b5ea3cd372e7fa7761e99383b30155
Cesar Gomez <zootechdrum@gmail.com>
2020-02-14 21:28:12 -0800

    imported pokemon to app.js file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
 import Wrapper from "./components/Wrapper";
+import pokemon from "./pokemon.json";
 import './App.css';
 
 function App() {
