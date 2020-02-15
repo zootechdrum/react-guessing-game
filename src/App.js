10401ba3b80d6cf489cade1a7048b8e46af4ec0b
Cesar Gomez <zootechdrum@gmail.com>
2020-02-15 10:44:36 -0800

    formatted code

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
               type={pokemon.type}
               weakness={pokemon.weakness}
             />
-                </div>
+        </div>
           ))}
 
         </div>
