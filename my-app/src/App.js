6dfd3d974749294f8e429767eceecb0f1d8d2017
Cesar Gomez <zootechdrum@gmail.com>
2020-02-14 21:24:16 -0800

    added container component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
+import Wrapper from "./components/Wrapper";
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
     <Navbar />
     <Jumbotron />
+    <Wrapper />
 
     </div>
   );
