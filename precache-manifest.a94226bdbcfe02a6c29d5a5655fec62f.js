8b5b37883ebbf8e0ebb0b8701fb1defe8988b0d3
Cesar Gomez <zootechdrum@gmail.com>
2020-02-17 08:50:47 -0800

    Updates

---
+++ precache-manifest.a94226bdbcfe02a6c29d5a5655fec62f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25c59e77104e8d91d8f8058c296a62c",
+    "url": "/react-guessing-game/index.html"
+  },
+  {
+    "revision": "d51ac37b6a80045ffa07",
+    "url": "/react-guessing-game/static/css/main.bd716bd3.chunk.css"
+  },
+  {
+    "revision": "87ccfea1f8602a6007c5",
+    "url": "/react-guessing-game/static/js/2.dcff481c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-guessing-game/static/js/2.dcff481c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51ac37b6a80045ffa07",
+    "url": "/react-guessing-game/static/js/main.983315b9.chunk.js"
+  },
+  {
+    "revision": "0e786d48b16845f32c2e",
+    "url": "/react-guessing-game/static/js/runtime-main.107174d4.js"
+  },
+  {
+    "revision": "8e4eb169831e841d7b4a0cb107cf384b",
+    "url": "/react-guessing-game/static/media/pokeball.8e4eb169.png"
+  },
+  {
+    "revision": "e0133029b93507c8f3af162309bc476d",
+    "url": "/react-guessing-game/static/media/pokemon-ban.e0133029.png"
+  },
+  {
+    "revision": "cabe89fce931c26c034460e475bf8544",
+    "url": "/react-guessing-game/static/media/pokemon-logo-footer1.cabe89fc.png"
+  }
+]);
